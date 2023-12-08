887e21f093d70dfb26a278a816ecbf45b0d40937
Yasin <yasincaykur@gmail.com>
2023-12-09 00:21:05 +0300

    added day3

--- 2015/python/day3.py
+++ 2015/python/day3.py
@@ -27,7 +27,35 @@ def part1(data: str) -> int:
     return len(visited)
 
 def part2(data: str) -> int:
-    return 0
+    santa_x = 0
+    santa_y = 0
+    robot_x = 0
+    robot_y = 0
+    santa_location = {(santa_x,santa_y)}
+
+    for key, ch in enumerate(data):
+        if key % 2 == 0:
+            if ch == "^":
+                santa_y += 1
+            elif ch == ">":
+                santa_x += 1
+            elif ch == "v":
+                santa_y -= 1
+            elif ch == "<":
+                santa_x -= 1
+            santa_location.add((santa_x, santa_y))
+        else:
+            if ch == "^":
+                robot_y += 1
+            elif ch == ">":
+                robot_x += 1
+            elif ch == "v":
+                robot_y -= 1
+            elif ch == "<":
+                robot_x -= 1
+            santa_location.add((robot_x, robot_y))
+
+    return len(santa_location)
 
 
 
@@ -42,10 +70,13 @@ class TestDay3(unittest.TestCase):
         self.assertEqual(2565, part1(_read_file()))
 
     def test_part2_empty_input(self):
-        self.assertEqual(0, part2(""))
+        self.assertEqual(1, part2(""))
 
     def test_part2_sample_input(self):
-        self.assertEqual(3, part2("^v"))
+        self.assertEqual(11, part2("^v^v^v^v^v"))
+
+    def test_part2(self):
+        self.assertEqual(2639, part2(_read_file()))
 
 
 if __name__ == "__main__":

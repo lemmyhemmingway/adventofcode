060e9b17fd7d17fba0c20717e75c9867caa60d97
Yasin <yasincaykur@gmail.com>
2023-12-08 14:22:29 +0300

    initial commit

--- elixir/aoc2015/test/aoc2015_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Aoc2015Test do
-  use ExUnit.Case
-  doctest Aoc2015
-
-  test "greets the world" do
-    assert Aoc2015.hello() == :world
-  end
-end
--- py/tests/aoc2015/test_day4.py
+++
@@ -1,9 +0,0 @@
-from aoc2015 import day4
-
-def test_part1():
-    result = day4.part1()
-    assert result == 117946
-
-def test_part2():
-    result = day4.part2()
-    assert result == 3938038
